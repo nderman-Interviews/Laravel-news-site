bf9966ddee5ad5c938ef8965669586f0adc4e2f7
Neal Derman <neal@travelground.com>
2017-12-07 15:33:09 +0200

    create edit article link

--- resources/views/snippet.blade.php
+++ resources/views/snippet.blade.php
@@ -1,8 +1,19 @@
 <li class="list-group-item">
-<div class="snippet">
-	<div class="h2"><a href="{{url('/'.$article->id)}}">{{ $article->title }}</a></div>
-	<div class="h4">{{$article->author->name}}</div>
-	{{-- 	<div class="snippet-image"> {{ $image }} </div> --}}
+<div class="snippet row">
+	<div class="col-xs-12">
+		<div class="h2 row">
+			<a class="col-xs-9" href="{{url('/'.$article->id)}}">{{ $article->title }}</a>
+			<div class="col-xs-3"> <a href="{{url('/edit/'.$article->id)}}"><button>Edit</button> </a></div>
+		</div>
+
+	</div>
+
+	<div class="col-xs-12">
+		<div class="h4">{{$article->author->name}}  </div>
+	</div>
+
+	<div class="col-xs-12">
 		<div class="snippet-text"> {{$article->snippet_text}}</div>
+	</div>
 </div>
 </li>
