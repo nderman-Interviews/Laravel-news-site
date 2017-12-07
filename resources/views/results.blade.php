9cc5112892981e9f83535a469c4dabdda6b08dc7
Neal Derman <neal@travelground.com>
2017-12-07 15:42:58 +0200

    new article link

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('content')
+	<div class="panel-default">
+        <div class="h2"><a href="{{url('/new')}}"><button>New Article</button></a></div>
+    </div>
     @foreach ($articles as $article)
     	<ul class="list-group">
     	@include('snippet', ['artilce' => $article])
